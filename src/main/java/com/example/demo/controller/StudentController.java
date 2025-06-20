f71e7b823686a5397491ed5994cf8731c2490f40
quanghoatrinh <quanghoa.trinh@gmail.com>
2025-06-20 10:00:34 +0700

    demo create students

--- src/main/java/com/example/demo/controller/StudentController.java
+++ src/main/java/com/example/demo/controller/StudentController.java
@@ -5,6 +5,8 @@ import com.example.demo.repository.StudentRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -24,5 +26,17 @@ public class StudentController {
         model.addAttribute("students",list);
         model.addAttribute("content","students/list");
         return "layout/main";
+    }
+
+    @GetMapping("create")
+    public String formCreate(Model model){
+        model.addAttribute("content","students/create_form");
+        return "layout/main";
+    }
+
+    @PostMapping("create")
+    public String createStudent(@ModelAttribute Student student){
+        studentRepository.save(student);
+        return "redirect:/students";
     }
 }
